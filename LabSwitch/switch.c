1453a3ed582dda7714606f199d27a920b865e1be
mgpauly1548 <mgpauly@hawaii.edu>
2023-03-22 02:27:12 -1000

    ping works, can't get dl up to work completely, probably missing some job data that should be in the packet

--- LabSwitch/switch.c
+++ LabSwitch/switch.c
@@ -65,29 +65,10 @@ void init_forward_table(struct forward_table *table) {
 }
 
 void add_src_to_table(struct forward_table *table, struct packet *pkt, int port_index) {
-   
-// every time 0 sends something, it will start at 0 and get it right, which will screw it up
-   int i;
-   for(i = 0; i < MAX_TABLE_SIZE; i++) {
-   if( (char)table->HostID[i] == pkt->src ) {
-      if(table->valid[i]) {
-         return;
-      } else {
-         table->valid[i] = 1;
-         table->HostID[i] = pkt->src;
-         table->port[i] = port_index;
-      }
-   }
-
-  }
-
-   int s = table->size;
-   table->valid[s] = 1;
-   table->HostID[i] = pkt->src;
-   table->port[i] = port_index;
-   table->size = table->size + 1;
-
-
+   if (table->valid[(int)pkt->src]) return;
+   table->valid[(int)pkt->src] = 1;
+   table->HostID[(int)pkt->src] = pkt->src;
+   table->port[(int)pkt->src] = port_index;
 
 }
 
@@ -100,13 +81,7 @@ void send_to_all_ports(int node_port_num, struct net_port **node_port, struct pa
 
 
 int is_host_in_table(struct forward_table *table, char dst) {
-   int i;
-   for(i = 0; i < MAX_TABLE_SIZE; i++) {
-      if( (char)table->HostID[i] == dst && table->valid[i]) {
-         return i;
-   }
-      return -1;
-}
+   return table->valid[(int)dst];
 }
 
 void switch_main(int host_id) {
@@ -159,9 +134,9 @@ void switch_main(int host_id) {
             display_forward_table(table);
             // add packet routing here
             // check whole table
-            if((n = is_host_in_table(&table, in_packet->dst)) >= 0) {
+            if(is_host_in_table(&table, in_packet->dst)) {
                // port is in table, send it
-               packet_send(node_port[n], in_packet);
+               packet_send(node_port[table.port[in_packet->dst]], in_packet);
                add_src_to_table(&table, in_packet, k);
 
             } else {
